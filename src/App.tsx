f1299eab920541ccb4851c03fe400d63060caca5
Ammar Sabit <ammarsabit39@gmail.com>
2025-07-21 10:12:00 +0300

    refactor: reorganize color button components and remove unused InputHex component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import ColorPreview from "./components/ColorPreview";
 import ColorButton from "./components/ColorButton";
-import InputHex from "./components/InputHex";
-import { useState } from "react";
-import "./App.css"
+import { useRef, useState } from "react";
+import "./App.css";
 
 function App() {
-  const [color, setColor] = useState('white')
+  const [color, setColor] = useState("white");
+  const inputRef = useRef<HTMLInputElement>(null);
   return (
     <>
       <h1>Color Palette Picker</h1>
@@ -14,15 +14,52 @@ function App() {
         <ColorPreview color={color} />
       </div>
       <div className="btn-container">
-        <ColorButton color="Violet" onClick={() => setColor('Violet')} btnName="Violet"></ColorButton>
-        <ColorButton color="Indigo" onClick={() => setColor('Indigo')} btnName="Indigo"></ColorButton>
-        <ColorButton color="Blue" onClick={() => setColor('Blue')} btnName="Blue"></ColorButton>
-        <ColorButton color="Green" onClick={() => setColor('Green')} btnName="Green"></ColorButton>
-        <ColorButton color="Yellow" onClick={() => setColor('Yellow')} btnName="Yellow"></ColorButton>
-        <ColorButton color="Orange" onClick={() => setColor('Orange')} btnName="Orange"></ColorButton>
-        <ColorButton color="Red" onClick={() => setColor('Red')} btnName="Red"></ColorButton>
-        <ColorButton color="white" onClick={() => setColor('')} btnName="Reset"></ColorButton>
-        <InputHex />
+        <ColorButton
+          color="Violet"
+          onClick={() => setColor("Violet")}
+          btnName="Violet"
+        ></ColorButton>
+        <ColorButton
+          color="Indigo"
+          onClick={() => setColor("Indigo")}
+          btnName="Indigo"
+        ></ColorButton>
+        <ColorButton
+          color="Blue"
+          onClick={() => setColor("Blue")}
+          btnName="Blue"
+        ></ColorButton>
+        <ColorButton
+          color="Green"
+          onClick={() => setColor("Green")}
+          btnName="Green"
+        ></ColorButton>
+        <ColorButton
+          color="Yellow"
+          onClick={() => setColor("Yellow")}
+          btnName="Yellow"
+        ></ColorButton>
+        <ColorButton
+          color="Orange"
+          onClick={() => setColor("Orange")}
+          btnName="Orange"
+        ></ColorButton>
+        <ColorButton
+          color="Red"
+          onClick={() => setColor("Red")}
+          btnName="Red"
+        ></ColorButton>
+        <ColorButton
+          color="white"
+          onClick={() => setColor("")}
+          btnName="Reset"
+        ></ColorButton>
+        <div className="input-container">
+          <input type="text" placeholder="Enter Hex value" ref={inputRef} />
+          <button onClick={() => setColor("#" + inputRef.current?.value)}>
+            Set Color
+          </button>
+        </div>
       </div>
     </>
   );
